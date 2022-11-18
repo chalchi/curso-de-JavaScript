365446a90e9d9345f9e1fb99a594ca3b371531f4
chalchi <maguilarom@gmail.com>
2022-11-18 10:50:47 -0600

    carpeta 1 de tareas completo

--- 1/1234ejercicios.js
+++ 1/1234ejercicios.js
@@ -1,5 +1,4 @@
 // Esta función cuenta el número de caracteres de una cadena de texto sin contar los espacios. pe.miFuncion('Hola mundo'); devolvera 9.
-
 function separador(string) {
   if (!string) return "No has escrito nada de nada";
   if (string) {
@@ -11,11 +10,8 @@ function separador(string) {
     return cont + " caracteres hay en la palabra " + string + ".";
   }
 }
-
 // -Esta función devuelve el texto recortado según el número de caracteres indicados, pe.miFumcion('Hola mundo', 4); devolverá 'Hola'.
-
-let palabra =
-  "si los monitos son tan lindos como tu entonces amaria trabajar en el sologico";
+let palabra = "Cualquier otra cosa que no este tan larga";
 function recorte(string, num) {
   let acumulador = [];
   for (let i = 0; i < string.length; i++) {
@@ -26,15 +22,22 @@ function recorte(string, num) {
   }
 }
 // console.log(recorte(palabra,20));
-
 // -Esta función le insertas un String y te devuelva un Array de textos separados por cierto caracter,  pe.miFumcion ('Hola que tal ',''); devolverá ['Hola', 'que', 'tal',].
-
 function separaTextos(array, caracter) {
   if (caracter) {
     let separado = array.split(" ").join(caracter);
     return separado;
   }
 }
-console.log(separaTextos("Me lleva", ". "));
-
-// -Programa una función que repita un texto X veces, pe.miFuncion('Hola Mundo', 3); devolvera  Hola Mundo Hola Mundo Hola Mundo.
+// console.log(separaTextos("Me lleva", ". "));
+// -Esta función repite un texto X veces, pe.miFuncion('Hola Mundo', 3); devolvera  Hola Mundo Hola Mundo Hola Mundo.
+function repetidor(string, veces) {
+  let tuna = [];
+  for (let i = 0; i < veces; i++) {
+    console.log(string);
+    tuna.push(string);
+    console.log(tuna);
+  }
+  return tuna;
+}
+console.log(repetidor("cocobongo", 3));
